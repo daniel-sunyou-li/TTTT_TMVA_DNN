2f1826dc22ce7ec8bf869d53b65cc717f34730d7
Daniel Li <45447223+daniel-sunyou-li@users.noreply.github.com>
2019-12-19 16:10:22 -0500

    Update doCondorVariableImportance.sh

--- doCondorVariableImportance.sh
+++ doCondorVariableImportance.sh
@@ -9,4 +9,4 @@ source /cvmfs/sft.cern.ch/lcg/contrib/gcc/7.3.0/x86_64-centos7-gcc7-opt/setup.sh
 source /cvmfs/sft.cern.ch/lcg/app/releases/ROOT/6.16.00/x86_64-centos7-gcc48-opt/bin/thisroot.sh
 
 #python TMVAClassification_VarImportanceDNN.py -m Keras -i "TTTT_TuneCP5_PSweights_13TeV-powheg-pythia8_hadd.root" -s 10
-python doCondorVariableImportanceDNN.py
+python doCondorVariableImportanceDNN.py -o 1
