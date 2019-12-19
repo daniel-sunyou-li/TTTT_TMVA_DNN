bfecab34de4a31879ca7d84b281eca766eb873d7
Daniel Li <45447223+daniel-sunyou-li@users.noreply.github.com>
2019-12-19 08:59:04 -0500

    Update doCondorVariableImportanceWrapper.sh

--- doCondorVariableImportanceWrapper.sh
+++ doCondorVariableImportanceWrapper.sh
@@ -2,7 +2,7 @@
 
 runDir=${1}
 method=${2}
-vListKey=${3}
+tag=${3}
 str_xbitset=${4}
 
 source /cvmfs/cms.cern.ch/cmsset_default.sh
@@ -16,4 +16,4 @@ eval `scramv1 runtime -sh`
 source /cvmfs/sft.cern.ch/lcg/contrib/gcc/7.3.0/x86_64-centos7-gcc7-opt/setup.sh
 source /cvmfs/sft.cern.ch/lcg/app/releases/ROOT/6.16.00/x86_64-centos7-gcc48-opt/bin/thisroot.sh
 
-python TMVAClassification_VarImportanceDNN.py -m $method -i "TTTT_TuneCP5_PSweights_13TeV-powheg-pythia8_hadd.root" -s $str_xbitset -l $vListKey
+python TMVAClassification_VarImportanceDNN.py -m $method -i "TTTT_TuneCP5_PSweights_13TeV-powheg-pythia8_hadd.root" -s $str_xbitset -t $tag
