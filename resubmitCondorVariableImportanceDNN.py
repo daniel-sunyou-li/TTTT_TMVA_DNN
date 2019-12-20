4b510bfd0c53382a1b4a85f1953a09710de53726
Daniel Li <45447223+daniel-sunyou-li@users.noreply.github.com>
2019-12-20 13:59:15 -0500

    Update resubmitCondorVariableImportanceDNN.py

--- resubmitCondorVariableImportanceDNN.py
+++ resubmitCondorVariableImportanceDNN.py
@@ -6,7 +6,7 @@ import varsList
 outPath = os.getcwd()
 outFile = os.listdir(outPath + '/condor_log/')
 
-numVars = 11
+numVars = len(varsList.varList["BigComb"])
 method = "Keras"
 condorDir = outPath + "/condor_log/"
 os.system("mkdir -p " + condorDir)
@@ -29,20 +29,20 @@ seedResubmit = []     # seeds to resubmit
 subseedResubmit = {}  # subseeds to resubmit
 
 for index, seed in enumerate(seedList):
-  seedDict[seed] = glob.glob("Keras_BigComb_" + str(numVars) + "vars_Seed_" + seed + "_Subseed_*.out")
+  seedDict[seed] = glob.glob("Keras_" + str(numVars) + "vars_Seed_" + seed + "_Subseed_*.out")
   
 for seed in seedDict:
   write_bool = True
-  for line in open("Keras_BigComb_" + str(numVars) + "vars_Seed_" + seed + ".out").readlines():
-    if "DONE" in line: write_bool = False
-  if write_bool == True: # and os.stat("Keras_BigComb_" + str(numVars) + "vars_Seed_" + seed + ".out").st_size > 0:
+  for line in open("Keras_" + str(numVars) + "vars_Seed_" + seed + ".out").readlines():
+    if "ROC-integral" in line: write_bool = False
+  if write_bool == True: and os.stat("Keras_" + str(numVars) + "vars_Seed_" + seed + ".out").st_size > 5:
     seedResubmit.append(seed)
   for subseedout in seedDict[seed]:
     subseed = subseedout.split("_Subseed_")[1].split(".out")[0]
     write_bool = True
-    for line in open("Keras_BigComb_" + str(numVars) + "vars_Seed_" + seed + "_Subseed_" + subseed + ".out").readlines():
-      if "DONE" in line: write_bool = False
-    if write_bool == True: # and os.stat("Keras_BigComb_" + str(numVars) + "vars_Seed_" + seed + "_Subseed_" + subseed + ".out").st_size != 0:
+    for line in open("Keras_" + str(numVars) + "vars_Seed_" + seed + "_Subseed_" + subseed + ".out").readlines():
+      if "ROC-integral" in line: write_bool = False
+    if write_bool == True and os.stat("Keras_" + str(numVars) + "vars_Seed_" + seed + "_Subseed_" + subseed + ".out").st_size > 5:
       if seed in subseedResubmit.keys():
         subseedResubmit[seed].append(subseed)
       else:
@@ -52,13 +52,14 @@ for seed in seedDict:
 
 os.chdir(outPath)
 
+count = 0
 for seed in seedResubmit:
   outf_key = "Seed_" + str(seed)
-  fileName = method + "_" + "BigComb" + "_" + str(len(varList)) + "vars_" + outf_key
+  fileName = "Keras_" + str(len(varList)) + "vars_" + outf_key
   dict = {
     "RUNDIR":outPath,
     "METHOD":method,
-    "vListKey":"BigComb",
+    "TAG":"str(count),
     "SeedN":seed,
     "FILENAME":fileName
   }
@@ -70,12 +71,12 @@ for seed in seedResubmit:
 Executable = %(RUNDIR)s/doCondorVariableImportanceWrapper.sh
 Should_Transfer_Files = YES
 WhenToTransferOutput = ON_EXIT
-request_memory = 3072
+request_memory = 4025
 Output = %(FILENAME)s.out
 Error = %(FILENAME)s.err
 Log = %(FILENAME)s.log
 Notification = Never
-Arguments = %(RUNDIR)s %(METHOD)s %(vListKey)s %(SeedN)s
+Arguments = %(RUNDIR)s %(METHOD)s %(TAG)s %(SeedN)s
 Queue 1"""%dict
   )
   jdf.close()
@@ -83,15 +84,16 @@ Queue 1"""%dict
   os.system("condor_submit %(FILENAME)s.job"%dict)
   os.system("sleep 0.5")
   os.chdir("%s"%(outPath))
-
+count += 1
+  
 for seed in subseedResubmit:
   for subseed in subseedResubmit[seed]:
     outf_key = "Seed_" + str(seed) + "_Subseed_" + str(subseed)
-    fileName = method + "_" + "BigComb" + "_" + str(len(varList)) + "vars_" + outf_key
+    fileName = "Keras_" + str(len(varList)) + "vars_" + outf_key
     dict_sub = {
       "RUNDIR":outPath,
       "METHOD":method,
-      "vListKey":"BigComb",
+      "TAG":str(count),
       "SeedN":seed,
       "SubSeedN":subseed,
       "FILENAME":fileName
@@ -104,12 +106,12 @@ for seed in subseedResubmit:
 Executable = %(RUNDIR)s/doCondorVariableImportanceWrapper.sh
 Should_Transfer_Files = YES
 WhenToTransferOutput = ON_EXIT
-request_memory = 3072
+request_memory = 4025
 Output = %(FILENAME)s.out
 Error = %(FILENAME)s.err
 Log = %(FILENAME)s.log
 Notification = Never
-Arguments = %(RUNDIR)s %(METHOD)s %(vListKey)s %(SubSeedN)s
+Arguments = %(RUNDIR)s %(METHOD)s %(TAG)s %(SubSeedN)s
 Queue 1"""%dict_sub
     )
     jdf.close()
@@ -117,3 +119,4 @@ Queue 1"""%dict_sub
     os.system("condor_submit %(FILENAME)s.job"%dict_sub)
     os.system("sleep 0.5")
     os.chdir("%s"%(outPath))
+    count += 1
