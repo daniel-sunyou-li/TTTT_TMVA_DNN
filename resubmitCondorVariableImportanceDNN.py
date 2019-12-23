05861ad1547626f5ddf1bd9818c547743f6dafab
Daniel Li <45447223+daniel-sunyou-li@users.noreply.github.com>
2019-12-23 09:41:19 -0500

    Update resubmitCondorVariableImportanceDNN.py
    changed implementation and adjusted the conditions for checking the resubmission

--- resubmitCondorVariableImportanceDNN.py
+++ resubmitCondorVariableImportanceDNN.py
@@ -3,128 +3,26 @@ import glob, os, sys
 import math
 import varsList
 
-outPath = os.getcwd()
-outFile = os.listdir(outPath + '/condor_log/')
-
-numVars = len(varsList.varList["BigComb"])
-method = "Keras"
-condorDir = outPath + "/condor_log/"
-os.system("mkdir -p " + condorDir)
-
-varListKeys = ["BigComb"]
-varList = varsList.varList["BigComb"]
-
-seedList = []
-
-for Files in outFile:
-  if "Subseed_" not in Files and ".out" in Files:
-    seed_number = Files.split("_Seed_")[1].split(".out")[0]
-    seedList.append(seed_number)
-    
-os.chdir(outPath+'/condor_log')
-
-seedDict = {}         # seeds are keys and subseeds are entries
-seedResubmit = []     # seeds to resubmit
-subseedResubmit = {}  # subseeds to resubmit
-
-for index, seed in enumerate(seedList):
-  seedDict[seed] = glob.glob("Keras_" + str(numVars) + "vars_Seed_" + seed + "_Subseed_*.out")
-  
-for seed in seedDict:
-  fileName = "Keras_" + str(numVars) + "vars_Seed_" + seed
-  check_one = True # both check one and check two have to be true to submit a new job
-  check_two = False
-  for line in open(fileName + ".out").readlines():
-    if "ROC-integral" in line:
-      check_one = False
-      print("Deleting {} with file size: {}".format(fileName+".out",os.stat(condorDir + fileName + ".out").st_size))
-      os.system("rm {}".format(condorDir + fileName + ".out"))
-      break
-  for line in open("Keras_" + str(numVars) + "vars_Seed_" + seed + ".log").readlines():
-    if "005" in line: 
-      check_two = True
-      os.system("rm {}".format(condorDir + fileName + ".log"))
-      break
-  if check_one == True and check_two == True:
-    seedResubmit.append(seed)
-  for subseedout in seedDict[seed]:
-    subseed = subseedout.split("_Subseed_")[1].split(".out")[0]
-    fileName = "Keras_" + str(numVars) + "vars_Seed_" + seed + "_Subseed_" + subseed
-    check_one = True
-    check_two = False
-    for line in open(fileName + ".out").readlines():
-      if "ROC-integral" in line:
-        check_one = False
-        print("Deleting {} with file size: {}".format(fileName + ".out",os.stat(condorDir + fileName + ".out").st_size))
-        os.system("rm {}".format(condorDir + fileName + ".out"))
-        break
-    for line in open(fileName + ".log").readlines():
-      if "005" in line:
-        check_two = True
-        os.system("rm {}".format(condorDir + fileName + ".log"))
-        break
-    if check_one == True and check_two == True:
-      if seed in subseedResubmit.keys():
-        subseedResubmit[seed].append(subseed)
-      else:
-        subseedResubmit[seed] = [subseed]
-      
-# Submit the condor jobs
-
-os.chdir(outPath)
-
-count = 0
-for seed in seedResubmit:
-  outf_key = "Seed_" + str(seed)
-  fileName = "Keras_" + str(len(varList)) + "vars_" + outf_key
-  count += 1
-  print("Job {} submitted.".format(count))
-  dict = {
-    "RUNDIR":outPath,
-    "METHOD":method,
-    "TAG":str(count),
-    "SeedN":seed,
-    "FILENAME":fileName
-  }
-  jdfName = condorDir + "%(FILENAME)s.job"%dict
-  print(jdfName)
-  jdf = open(jdfName, "w")
-  jdf.write(
-"""universe = vanilla
-Executable = %(RUNDIR)s/doCondorVariableImportanceWrapper.sh
-Should_Transfer_Files = YES
-WhenToTransferOutput = ON_EXIT
-request_memory = 4025
-Output = %(FILENAME)s.out
-Error = %(FILENAME)s.err
-Log = %(FILENAME)s.log
-Notification = Never
-Arguments = %(RUNDIR)s %(METHOD)s %(TAG)s %(SeedN)s
-Queue 1"""%dict
-  )
-  jdf.close()
-  os.chdir("%s/"%(condorDir))
-  os.system("condor_submit %(FILENAME)s.job"%dict)
-  os.system("sleep 0.5")
-  os.chdir("%s"%(outPath))
-  
-for seed in subseedResubmit:
-  for subseed in subseedResubmit[seed]:
-    outf_key = "Seed_" + str(seed) + "_Subseed_" + str(subseed)
-    fileName = "Keras_" + str(len(varList)) + "vars_" + outf_key
-    count += 1
-    print("Job {} submitted.".format(count))
-    dict_sub = {
-      "RUNDIR":outPath,
-      "METHOD":method,
-      "TAG":str(count),
-      "SeedN":seed,
-      "SubSeedN":subseed,
-      "FILENAME":fileName
+def condorJob(SeedN="",SubSeedN="",count=0,options=['','','']): # submits a single condor job
+    runDir = options[0]
+    condorDir = options[1]
+    numVars = options[2]
+    SubmitSeedN = ""
+    if SubSeedN == "": 
+        fileName = "Keras_" + str(numVars) + "vars_Seed_" + str(SeedN)
+        SubmitSeedN = SeedN
+    else: 
+        fileName = "Keras_" + str(numVars) + "vars_Seed_" + str(SeedN) + "_Subseed_" + str(SubSeedN)
+        SubmitSeedN = SubSeedN
+    dict = {
+        "RUNDIR": runDir,           # run directory
+        "METHOD": "Keras",          # tmva method, should be "Keras"
+        "SubmitSeedN": SubmitSeedN,
+        "TAG": str(count),
+        "FILENAME": fileName
     }
-    jdfName = condorDir + "%(FILENAME)s.job"%dict_sub
-    print(jdfName)
-    jdf = open(jdfName,"w")
+    jdfName = condorDir + "%(FILENAME)s.job"%dict
+    jdf = open(jdfName, "w")
     jdf.write(
 """universe = vanilla
 Executable = %(RUNDIR)s/doCondorVariableImportanceWrapper.sh
@@ -135,11 +33,81 @@ Output = %(FILENAME)s.out
 Error = %(FILENAME)s.err
 Log = %(FILENAME)s.log
 Notification = Never
-Arguments = %(RUNDIR)s %(METHOD)s %(TAG)s %(SubSeedN)s
-Queue 1"""%dict_sub
-    )
+Arguments = %(RUNDIR)s %(METHOD)s %(TAG)s %(SubmitSeedN)s
+Queue 1"""%dict)
     jdf.close()
     os.chdir("%s/"%(condorDir))
-    os.system("condor_submit %(FILENAME)s.job"%dict_sub)
+    os.system("condor_submit %(FILENAME)s.job"%dict)
     os.system("sleep 0.5")
-    os.chdir("%s"%(outPath))
+    os.chdir("%s"%(runDir))
+    
+    count += 1
+    print("{} jobs submitted.".format(count))
+    return count
+    
+options = [
+    os.getcwd(),
+    os.getcwd() + "/condor_log/",
+    len(varsList.varList["BigComb"]
+]
+
+count = 0
+
+seedList = []           # holds all seed keys
+seedDict = {}           # seeds are the key and subseeds are the entries
+seedResubmit = []       # seeds to resubmit
+subseedResubmit = {}    # subseeds to resubmit where seeds are the key
+
+seedStrDir = os.listdir(options[1])
+
+for seedStr in seedStrDir:
+    if "Subseed_" not in seedStr and ".out" in seedStr:
+        seed = seedStr.split("_Seed_")[1].split(".out")[0]
+        seedList.append(seed)
+        
+for seed in seedList:
+    seedDict[seed] = glob.glob("Keras_" + str(numVars) + "vars_Seed_" + seed + "_Subseed_*.out")
+
+maxSeed = str(int("1"*options[2],2))
+formSize = max(len(maxSeed) + 1, 8)
+print("{:{}}{:{}}{:5}".format("Seed",formSize,"Subseed",formSize,"Size (b)"))
+# resubmit the seed jobs that failed
+for seed in seedDict:
+    fileName = "Keras_" + str(numVars) + "vars_Seed_" + seed
+    check_one = True        # checks if "ROC-integral" is in .out file
+    check_two = False       # checks if file is done running
+    
+    # perform first check on [fileName].out
+    for line in open(options[1] + fileName + ".out").readlines():
+        # don't want to resubmit if "ROC-integral" is already calculated
+        if "ROC-integral" in line: check_one = False
+    if check_one:
+        for line in open(options[1] + fileName + ".log").readlines():
+            # if 005 is the last condor status, then do resubmit
+            if "005" in line: check_two = True
+            # if 000 or 006, then the job is either in queue or running
+            elif "000" or "006" in line: check_two = False
+    if check_one and check_two:
+        fileSize = os.stat(options[1] + fileName + ".out").st_size
+        print("{:{}}{:{}}{:5}".format(seed,formSize,"",formSize,fileSize))
+        seedResubmit.append(seed)
+        #count = condorJob(seed=seed,count=count,options=options)
+    for subseedStr in seedDict[seed]:
+        subseed = subseedStr.split("_Subseed_")[1].split(".out")[0]
+        fileNameSS = fileName + "_Subseed_" + subseed
+        check_one = True
+        check_two = False
+        for line in open(fileNameSS + ".out").readlines():
+            if "ROC-integral" in line: check_one = False
+        if check_one:
+            for line in open(fileNameSS + ".log").readlines():
+                if "005" in line: check_two = True
+                elif "000" or "006" in line: check_two = False
+        if check_one and check_two:
+            fileSize = os.stat(options[1] + fileNameSS + ".out").st_size
+            print("{:{}}{:{}}{:5}".format(seed,formSize,subseed,formSize,fileSize))
+            if seed in subseedResubmit.keys():
+                subseedResubmit[seed].append(subseed)
+            else:
+                subseedResubmit[seed] = [subseed]
+            #count = condorJob(seed,subseed,count,options)
