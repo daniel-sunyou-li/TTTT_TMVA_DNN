a2c4a968a154de66c1364b428847dc971bfd3e1b
Daniel Li <45447223+daniel-sunyou-li@users.noreply.github.com>
2020-01-31 18:55:59 -0500

    Create VariableImportanceBRUX_step2.sh

---
+++ BRUX/VariableImportanceBRUX_step2.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+runDir=${1}
+method=${2}
+tag=${3}
+str_xbitset=${4}
+
+source /cvmfs/cms.cern.ch/cmsset_default.sh
+
+cd $runDir
+echo $runDir
+
+export SCRAM_ARCH=slc7_amd64_gcc630
+eval `scramv1 runtime -sh`
+
+source /cvmfs/sft.cern.ch/lcg/contrib/gcc/7.3.0/x86_64-centos7-gcc7-opt/setup.sh
+source /cvmfs/sft.cern.ch/lcg/app/releases/ROOT/6.16.00/x86_64-centos7-gcc48-opt/bin/thisroot.sh
+
+python ../TMVAClassification_VariableImportance.py -m $method -i "TTTT_TuneCP5_PSweights_13TeV-amcatnlo-pythia8_hadd.root" -s $str_xbitset -t $tag
