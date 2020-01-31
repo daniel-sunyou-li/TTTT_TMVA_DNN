9fb3cbec101074a4ac7f2abd94d8ddd0a1b9a16a
Daniel Li <45447223+daniel-sunyou-li@users.noreply.github.com>
2020-01-31 18:44:25 -0500

    Update and rename VariableImportanceLPC_step1.sh to VariableImportanceBRUX_step1.sh

--- BRUX/VariableImportanceBRUX_step1.sh
+++ BRUX/VariableImportanceBRUX_step1.sh
@@ -9,4 +9,4 @@ eval `scramv1 runtime -sh`
 source /cvmfs/sft.cern.ch/lcg/contrib/gcc/7.3.0/x86_64-centos7-gcc7-opt/setup.sh
 source /cvmfs/sft.cern.ch/lcg/app/releases/ROOT/6.16.00/x86_64-centos7-gcc48-opt/bin/thisroot.sh
 
-python VariableImportanceLPC_step2.py
+python VariableImportanceBRUX_step2.py
