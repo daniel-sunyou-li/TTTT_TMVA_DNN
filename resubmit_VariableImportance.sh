0b53bbf4fc6ee78c9e7854a5749553ac767c0c54
Daniel Li <45447223+daniel-sunyou-li@users.noreply.github.com>
2020-02-02 15:05:41 -0500

    Create resubmit_VariableImportance.sh

---
+++ resubmit_VariableImportance.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+source /cvmfs/cms.cern.ch/cmsset_default.sh
+
+export SCRAM_ARCH=slc7_amd64_gcc630
+eval `scramv1 runtime -sh`
+
+source /cvmfs/sft.cern.ch/lcg/contrib/gcc/7.3.0/x86_64-centos7-gcc7-opt/setup.sh
+source /cvmfs/sft.cern.ch/lcg/app/releases/ROOT/6.16.00/x86_64-centos7-gcc48-opt/bin/thisroot.sh
+
+# python ./BRUX/VariableImportanceBRUX_resubmit.py # running on BRUX clusters
+python ./LPC/VariableImportanceLPC_resubmit.py     # running on LPC clusters, use this if input variables > 20 
