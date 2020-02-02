9d13efeb7e1e8ce8f971773e1123df9f0734d7f6
Daniel Li <45447223+daniel-sunyou-li@users.noreply.github.com>
2020-02-02 18:13:03 -0500

    Update resubmit_VariableImportance.sh

--- resubmit_VariableImportance.sh
+++ resubmit_VariableImportance.sh
@@ -8,5 +8,14 @@ eval `scramv1 runtime -sh`
 source /cvmfs/sft.cern.ch/lcg/contrib/gcc/7.3.0/x86_64-centos7-gcc7-opt/setup.sh
 source /cvmfs/sft.cern.ch/lcg/app/releases/ROOT/6.16.00/x86_64-centos7-gcc48-opt/bin/thisroot.sh
 
-# python ./BRUX/VariableImportanceBRUX_resubmit.py # running on BRUX clusters
-python ./LPC/VariableImportanceLPC_resubmit.py     # running on LPC clusters, use this if input variables > 20 
+host=${1}
+
+if [ $host == 'BRUX' ] || [ $host == 'brux' ] || [ $host == 'Brux' ] 
+then
+  python ./BRUX/VariableImportanceBRUX_resubmit.py # running on BRUX clusters
+elif [ $host == 'LPC' ] || [ $host == 'lpc' ] || [ $host == 'Lpc' ]
+then
+  python ./LPC/VariableImportanceLPC_resubmit.py   # running on LPC clusters, use this if input variables > 20
+else
+  echo Invalid or No Option Used. Choose [BRUX] or [LPC].
+fi
