d65cc0be7d51d3bcf7c3dd7659089db0421e3051
Daniel Li <45447223+daniel-sunyou-li@users.noreply.github.com>
2020-02-03 11:18:14 -0500

    Update VariableImportanceLPC_step2.sh

--- LPC/VariableImportanceLPC_step2.sh
+++ LPC/VariableImportanceLPC_step2.sh
@@ -1,9 +1,7 @@
 #!/bin/sh
 
-runDir=${1}
-method=${2}
-tag=${3}
-str_xbitset=${4}
+str_xbitset=${1}
+signalFile = "TTTT_TuneCP5_PSweights_13TeV-amcatnlo-pythia8_hadd.root" # 2017 TTTT Step 2 signal
 eosUserName="dsunyou" # change me
 
 source /cvmfs/cms.cern.ch/cmsset_default.sh
@@ -42,4 +40,4 @@ source /cvmfs/sft.cern.ch/lcg/app/releases/ROOT/6.16.00/x86_64-centos7-gcc48-opt
 
 cd ./TTTT_TMVA_DNN/
 
-python TMVAClassification_VariableImportance.py -m $method -i "TTTT_TuneCP5_PSweights_13TeV-amcatnlo-pythia8_hadd.root" -s $str_xbitset -t $tag
+python TMVAClassification_VariableImportance.py -i $signalFile -s $str_xbitset
