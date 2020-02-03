3727f5d703d921d450e386b31be7d4f3f8e08059
Daniel Li <45447223+daniel-sunyou-li@users.noreply.github.com>
2020-02-03 09:38:59 -0500

    Update countCondorJobs.py

--- countCondorJobs.py
+++ countCondorJobs.py
@@ -1,23 +1,44 @@
 #/usr/bin/env python
 import glob, os, sys
 import math
+import numpy as np
 import varsList
+
+def variable_occurence(count_arr, seed):
+  seed_str = "{:0{}b}".format(seed,len(count_arr))
+  for count, variable in enumerate(seed_str):
+    if variable == "1": count_arr[count] += 1
+  return count_arr
 
 seedDirectory = os.listdir(os.getcwd() + "/condor_log/")
 seedOutDirectory = [seedStr for seedStr in seedDirectory if ".out" in seedStr]
 
+numVars = int(seedOutDirectory[0].split("vars_")[0].split("Keras_")[1])
+count_arr = np.zeros(numVars)
+
 total_count = 0
-for seedStr in seedDirectory:
-  if ".job" in seedStr: total_count += 1
-  
 finished_count = 0
 
+# count total number of jobs submitted
+for seedStr in seedDirectory:
+  if ".job" in seedStr: total_count += 1
+
+# counts finished jobs and counts variable frequency in seed generation
 for seedOut in seedOutDirectory:
   for line in open(os.getcwd() + "/condor_log/" + seedOut).readlines():
     if "ROC-integral" in line:
       finished_count += 1
-      
+  if "Subseed" not in seedOut:
+    seed = int(seedOut.split("_Seed_")[1].split(".out")[0])
+    count_arr = variable_occurence(count_arr,seed)
+
+# display variable frequency 
+print("{:<3} {:<32} {:<6}".format("#","Variable Name","Count"))
+for i in range(numVars):
+  print("{:<3} {:<32} {:<6}".format(str(i)+".",varsList.varList["BigComb"][i][0],int(count_arr[i])))
+
+# display job status
 print("Finished condor jobs: {} / {}, {:.2f}%".format(
-  finished_count, total_count, 100 * float(finished_count) / float(total_count)
+  finished_count, total_count, 100. * float(finished_count) / float(total_count)
   )
 )
