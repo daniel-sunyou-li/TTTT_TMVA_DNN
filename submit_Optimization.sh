386e3346b5b77c29f5fe3ae8634b392aa5f5cfa5
Daniel Li <45447223+daniel-sunyou-li@users.noreply.github.com>
2020-02-05 13:31:27 -0500

    Update submit_Optimization.sh

--- submit_Optimization.sh
+++ submit_Optimization.sh
@@ -1,4 +1,5 @@
 #/bin/sh
+signalFile = "TTTT_TuneCP5_PSweights_13TeV-amcatnlo-pythia8_hadd.root" # 2017 TTTT Step 2 signal
 
 source /cvmfs/cms.cern.ch/cmsset_default.sh
 
@@ -8,4 +9,4 @@ eval `scramv1 runtime -sh`
 source /cvmfs/sft.cern.ch/lcg/contrib/gcc/7.3.0/x86_64-centos7-gcc7-opt/setup.sh
 source /cvmfs/sft.cern.ch/lcg/app/releases/ROOT/6.16.00/x86_64-centos7-gcc48-opt/bin/thisroot.sh
 
-python TMVAClassification_Optimization.py -i "TTTT_TuneCP5_PSweights_13TeV-powheg-pythia8_hadd.root"
+python TMVAClassification_Optimization.py -i $signalFile
