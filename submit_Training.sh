4d77ddc57797f50ce44482aae2063932392ca424
Daniel Li <45447223+daniel-sunyou-li@users.noreply.github.com>
2020-02-06 17:07:08 -0500

    Update submit_Training.sh

--- submit_Training.sh
+++ submit_Training.sh
@@ -1,5 +1,4 @@
 #/bin/sh
-signalFile = "TTTT_TuneCP5_PSweights_13TeV-amcatnlo-pythia8_hadd.root" # 2017 TTTT Step 2 signal
 
 source /cvmfs/cms.cern./ch/cmsset_default.sh
 
@@ -9,4 +8,4 @@ eval `scramv1 runtime -sh`
 source /cvmfs/sft.cern.ch/lcg/contrib/gcc/7.3.0/x86_64-centos7-gcc7-opt/setup.sh
 source /cvmfs/sft.cern.ch/lcg/app/releases/ROOT/6.16.00/x86_64-centos7-gcc48-opt/bin/thisroot.sh
 
-python TMVAClassification_Training.py -i $signalFile
+python TMVAClassification_Training.py
