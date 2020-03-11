49744da650228287efa9f123723a267cd8cc44c3
Daniel Li <45447223+daniel-sunyou-li@users.noreply.github.com>
2020-03-11 14:51:20 -0400

    Update varsList.py

--- varsList.py
+++ varsList.py
@@ -170,4 +170,4 @@ cutStr =  "( ( leptonPt_MultiLepCalc > 50 && isElectron == 1 ) || " + \
           "( theJetPt_JetSubCalc_PtOrdered[2] > 0 ) && " + \
           "( minDR_lepJet > 0.4 ) && " + \
           "( AK4HT > 510 ) && " + \
-          " DataPast == 1 && MCPastTrigger == 1 "
+          " DataPastTrigger == 1 && MCPastTrigger == 1 "
