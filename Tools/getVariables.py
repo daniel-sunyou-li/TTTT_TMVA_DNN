0878e6e97c1cc1e5b3105075ee30bb13c142fba9
Daniel Li <45447223+daniel-sunyou-li@users.noreply.github.com>
2020-03-17 13:37:01 -0400

    Update and rename getTTTTVariables.py to Tools/getVariables.py

--- Tools/getVariables.py
+++ Tools/getVariables.py
@@ -2,12 +2,14 @@ import numpy as np
 import os,sys
 import ROOT as root
 
-#file_path = "/mnt/hadoop/store/user/daili/TTTT/FWLJMET102X_1lep2017_4t_081019_step2hadds/nominal/TTTT_TuneCP5_PSweights_13TeV-powheg-pythia8_hadd.root"
+#file_path = "/mnt/hadoop/store/user/dali/TTTT/FWLJMET102X_1lep2017_4t_081019_step2hadds/nominal/TTTT_TuneCP5_PSweights_13TeV-powheg-pythia8_hadd.root"
 
-file_path = "/mnt/hadoop/store/group/bruxljm/FWLJMET102X_1lep2017_Oct2019_4t_12122019_step2/nominal/"
+production_path = "/mnt/hadoop/store/group/bruxljm"
+production_name = "/FWLJMET102X_1lep2017_Oct2019_4t_03032020_step2/nominal/"
 file_name = "TTTT_TuneCP5_PSweights_13TeV-amcatnlo-pythia8_hadd.root"
+file_path = production_path + production_name + file_name
 
-root_file = root.TFile.Open(file_path+file_name)
+root_file = root.TFile.Open(file_path)
 root_tree = root_file.Get("ljmet")
 
 branch_file = open(os.getcwd() + "/TTTT_variables.txt",'w')
