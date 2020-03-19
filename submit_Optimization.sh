b984cfe5924318828cdacaae497b15ff52bdead9
Daniel Li <45447223+daniel-sunyou-li@users.noreply.github.com>
2020-03-19 12:25:14 -0400

    Update submit_Optimization.sh

--- submit_Optimization.sh
+++ submit_Optimization.sh
@@ -1,19 +1,24 @@
-#/bin/csh
+#!/bin/sh
 
-#/bin/csh
+host=${1}
+numVars=${2}
 
-source /cvmfs/cms.cern.ch/cmsset_default.csh
-#source /cvmfs/cms.cern.ch/cmsset_default.sh
-
-setenv SCRAM_ARCH slc7_amd64_gcc630
-#export SCRAM_ARCH=slc7_amd64_gcc630
-
-eval `scramv1 runtime -csh`
-#eval `scramv1 runtime -sh`
-
-source /cvmfs/sft.cern.ch/lcg/contrib/gcc/7.3.0/x86_64-centos7-gcc7-opt/setup.csh
-source /cvmfs/sft.cern.ch/lcg/app/releases/ROOT/6.16.00/x86_64-centos7-gcc48-opt/bin/thisroot.csh
-#source /cvmfs/sft.cern.ch/lcg/contrib/gcc/7.3.0/x86_64-centos7-gcc7-opt/setup.sh
-#source /cvmfs/sft.cern.ch/lcg/app/releases/ROOT/6.16.00/x86_64-centos7-gcc48-opt/bin/thisroot.sh
-
-python TMVAClassification_OptimizationWrapper.py
+if [ $host == 'BRUX' ] || [ $host == 'brux' ] || [ $host == 'Brux' ]
+then
+  source /cvmfs/cms.cern.ch/cmsset_default.sh
+  export SCRAM_ARCH=slc7_amd64_gcc630
+  eval `scramv1 runtime -sh`
+  source /cvmfs/sft.cern.ch/lcg/contrib/gcc/7.3.0/x86_64-centos7-gcc7-opt/setup.sh
+  source /cvmfs/sft.cern.ch/lcg/app/releases/ROOT/6.16.00/x86_64-centos7-gcc48-opt/bin/thisroot.sh
+  python TMVAClassification_OptimizationWrapper brux $numVars
+elif [ $host == 'LPC' ] || [ $host == 'lpc' ] || [ $host == 'Lpc' ]
+then
+  source /cvmfs/cms.cern.ch/cmsset_default.csh
+  setenv SCRAM_ARCH slc7_amd64_gcc630
+  eval `scramv1 runtime -csh`
+  source /cvmfs/sft.cern.ch/lcg/contrib/gcc/7.3.0/x86_64-centos7-gcc7-opt/setup.csh
+  source /cvmfs/sft.cern.ch/lcg/app/releases/ROOT/6.16.00/x86_64-centos7-gcc48-opt/bin/thisroot.csh
+  python TMVAClassification_OptimizationWrapper.py lpc $numVars
+else
+  echo Invalid or No Options Used. Need to include "host" and "number of variables". Example submission: "./submit_Optimization.sh lpc 20".
+fi
