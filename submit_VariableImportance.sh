c83f337b025f87920d68e43b63e14db71a1034c9
Daniel Li <45447223+daniel-sunyou-li@users.noreply.github.com>
2020-04-13 10:55:11 -0400

    Update submit_VariableImportance.sh

--- submit_VariableImportance.sh
+++ submit_VariableImportance.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 
 host=${1}
-seeds=${2}
-corrCut=${3}
+year=${2}
+seeds=${3}
+corrCut=${4}
 
 source /cvmfs/cms.cern.ch/cmsset_default.sh
 
@@ -12,22 +13,21 @@ eval `scramv1 runtime -sh`
 source /cvmfs/sft.cern.ch/lcg/contrib/gcc/7.3.0/x86_64-centos7-gcc7-opt/setup.sh
 source /cvmfs/sft.cern.ch/lcg/app/releases/ROOT/6.16.00/x86_64-centos7-gcc48-opt/bin/thisroot.sh
 
-if [ $host == 'BRUX' ] || [ $host == 'brux' ] || [ $host == 'Brux' ] 
+if [ $host = 'BRUX' ] || [ $host = 'brux' ] || [ $host = 'Brux' ] && [ [ $year = '2017' ] || [ $year = '2018' ] ]
 then
-  if [ $seeds == '' ] || [ $corrCut == '' ]
-  then
-    python ./BRUX/VariableImportanceBRUX_step1.py # running on BRUX clusters
+  if [ $seeds = '' ] || [ $corrCut = '' ]
+    python ./BRUX/VariableImportanceBRUX_step1.py $year # running on BRUX clusters
   else
-    python ./BRUX/VariableImportanceBRUX_step1.py $seeds $corrCut
+    python ./BRUX/VariableImportanceBRUX_step1.py $year $seeds $corrCut
   fi
-elif [ $host == 'LPC' ] || [ $host == 'lpc' ] || [ $host == 'Lpc' ]
+elif [ $host = 'LPC' ] || [ $host = 'lpc' ] || [ $host = 'Lpc' ] && [ [ $year = '2017' ] || [ $year = '2018' ] ]
 then
-  if [ $seeds == '' ] || [ $corrCut == '' ]
+  if [ $seeds = '' ] || [ $corrCut = '' ]
   then
-    python ./LPC/VariableImportanceLPC_step1.py # running on LPC clusters, use this if input variables > 20 
+    python ./LPC/VariableImportanceLPC_step1.py $year # running on LPC clusters, use this if input variables > 20 
   else
-    python ./LPC/VariableImportanceLPC_step1.py $seeds $corrCut
+    python ./LPC/VariableImportanceLPC_step1.py $year $seeds $corrCut
   fi
 else
-  echo Invalid or No Option Used. Submit as "./submit_VariableImportance.sh BRUX 100 80" or "./submit_VariableImportance.sh LPC 100 80".
+  echo Invalid or No Option Used. Submit as "./submit_VariableImportance.sh BRUX 2017 100 80" or "./submit_VariableImportance.sh LPC 2017 100 80".
 fi
