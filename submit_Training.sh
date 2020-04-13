a4387d8dcc4f13b913d3fb82203876ebc2778d74
Daniel Li <45447223+daniel-sunyou-li@users.noreply.github.com>
2020-04-13 16:10:11 -0400

    Update submit_Training.sh

--- submit_Training.sh
+++ submit_Training.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 
 host=${1}
-option=${2}
-dataset=${3}
+year=${2}
+option=${3}
+dataset=${4}
 
 source /cvmfs/cms.cern.ch/cmsset_default.sh
 
@@ -13,23 +14,23 @@ eval `scramv1 runtime -sh`
 source /cvmfs/sft.cern.ch/lcg/contrib/gcc/7.3.0/x86_64-centos7-gcc7-opt/setup.sh
 source /cvmfs/sft.cern.ch/lcg/app/releases/ROOT/6.16.00/x86_64-centos7-gcc48-opt/bin/thisroot.sh
 
-if [ $host == 'BRUX' ] || [ $host == 'brux' ] || [ $host == 'Brux' ]
+if [ $host = 'BRUX' ] || [ $host = 'brux' ] || [ $host = 'Brux' ] && [ $year = '2017' ] || [ $year = '2018' ] ]
 then
-  if [ $option == '0' ]
+  if [ $option = '0' ]
   then
-    python TMVAClassification_Training.py -w brux -o 0 -d dataset
-  elif [ $option == '1' ]  
+    python TMVAClassification_Training.py -w brux -y $year -o 0 -d dataset
+  elif [ $option = '1' ]  
   then
-    python TMVAClassification_Training.py -w brux -o 1 -d $dataset
+    python TMVAClassification_Training.py -w brux -y $year -o 1 -d $dataset
   fi
-elif [ $host == 'LPC' ] || [ $host == 'lpc' ] || [ $host == 'Lpc' ]
+elif [ $host = 'LPC' ] || [ $host = 'lpc' ] || [ $host = 'Lpc' ] && [ [ $year = '2017' ] || [ $year = '2018' ] ] 
 then
-  if [ $option == '0' ]
+  if [ $option = '0' ]
   then
-    python TMVAClassification_Training.py -w lpc -o 0 -d dataset
-  elif [ $option == '1' ]
+    python TMVAClassification_Training.py -w lpc -y $year -o 0 -d dataset
+  elif [ $option = '1' ]
   then
-    python TMVAClassification_Training.py -w lpc -o 1 -d $dataset
+    python TMVAClassification_Training.py -w lpc -y $year -o 1 -d $dataset
   fi
 else
   echo Invalid or No Options Used.  Need to include "host" , "option" and "dataset"
