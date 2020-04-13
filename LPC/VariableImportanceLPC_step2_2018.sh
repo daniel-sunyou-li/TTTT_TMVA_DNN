3fa0c9c0eeb650e74adb387c7d753da18a643bb5
Daniel Li <45447223+daniel-sunyou-li@users.noreply.github.com>
2020-04-13 11:52:11 -0400

    Update VariableImportanceLPC_step2_2018.sh

--- LPC/VariableImportanceLPC_step2_2018.sh
+++ LPC/VariableImportanceLPC_step2_2018.sh
@@ -37,4 +37,4 @@ source /cvmfs/sft.cern.ch/lcg/app/releases/ROOT/6.16.00/x86_64-centos7-gcc48-opt
 
 cd ./TTTT_TMVA_DNN/
 
-python TMVAClassification_VariableImportance.py -s $str_xbitset -w lpc
+python TMVAClassification_VariableImportance.py -s $str_xbitset -w lpc -y 2018
