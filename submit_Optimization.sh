ac51c35e6bac2db2bcd46777466a61449206b1ee
Daniel Li <45447223+daniel-sunyou-li@users.noreply.github.com>
2020-04-25 11:37:07 -0400

    Update submit_Optimization.sh

--- submit_Optimization.sh
+++ submit_Optimization.sh
@@ -4,6 +4,7 @@ host=${1}
 year=${2}
 numVars=${3}
 option=${4}
+dataset=${5}
 
 source /cvmfs/cms.cern.ch/cmsset_default.sh
 
@@ -15,11 +16,11 @@ source /cvmfs/sft.cern.ch/lcg/app/releases/ROOT/6.16.00/x86_64-centos7-gcc48-opt
 
 if [ $host == 'BRUX' ] || [ $host == 'brux' ] || [ $host == 'Brux' ] && [ [ $year = '2017' ] || [ $year = '2018' ] ]
 then
-  python TMVAClassification_OptimizationWrapper -w brux -n $numVars -o $option -y $year
+  python TMVAClassification_OptimizationWrapper -w brux -n $numVars -o $option -y $year -d $dataset
 elif [ $host == 'LPC' ] || [ $host == 'lpc' ] || [ $host == 'Lpc' ] && [ [ $year = '2017' ] || [ $year = '2018' ] ]
 then
-  python TMVAClassification_OptimizationWrapper.py -w lpc -n $numVars -o $option -y $year
+  python TMVAClassification_OptimizationWrapper.py -w lpc -n $numVars -o $option -y $year -d $dataset
 else
   echo Invalid or No Options Used. Need to include "host" and "number of variables". 
-  echo Example submissions: "./submit_OptimizationWrapper.sh lpc 2017 20 1" or "./submit_OptimizationWrapper.sh brux 2018 20 0"
+  echo Example submissions: "./submit_OptimizationWrapper.sh lpc 2017 20 1 dataset" or "./submit_OptimizationWrapper.sh brux 2018 20 0 dataset"
 fi
