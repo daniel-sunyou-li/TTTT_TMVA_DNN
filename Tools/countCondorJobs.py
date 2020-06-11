cbbf7cb61e345db4c7b3dc91248631eec78685f0
Adam Furman <furmada@gmail.com>
2020-06-11 14:32:16 -0500

    Added command line options and seed count for countCondorJobs

--- Tools/countCondorJobs.py
+++ Tools/countCondorJobs.py
@@ -1,53 +1,65 @@
 #/usr/bin/env python
-import glob, os, sys
+import glob, os, sys, argparse
 import math
 import numpy as np
 sys.path.insert(0, "../TTTT_TMVA_DNN")
 import varsList
 
-def variable_occurence(count_arr, seed):
-  seed_str = "{:0{}b}".format(seed,len(count_arr))
-  for count, variable in enumerate(seed_str):
-    if variable == "1": count_arr[count] += 1
-  return count_arr
 
-def count_job(condorPath,seedOut,seedLog,seedOutDirectory,failed_count,finished_count):
-  if seedOut in seedOutDirectory:
-    job_success = False
-    for line in open(os.getcwd() + "/condor_log/" + seedOut).readlines():
-      if "ROC-integral" in line:
-        job_success = True
-        finished_count += 1
-        return failed_count, finished_count
-    if job_success == False:
-      failed_count += 1
-      print("{} failed to compute ROC-integral.".format(seedOut))
-      return failed_count, finished_count
-  else:
-    condor_done = False
-    for line in open(condorPath + seedLog).readlines():
-      if "005 (" or "009 (" in line: condor_done = True
-      if "006 (" or "000 (" or "001 (" in line: condor_done = False
-    if condor_done == False: return failed_count, finished_count
-    else:
-      failed_count += 1
-      print("{} failed to run.".format(seedLog))
-      return failed_count, finished_count
+parser = argparse.ArgumentParser()
+parser.add_argument("-f", "--folder", help="The condor log folder.")
+parser.add_argument("-v", "--verbose", action="store_true", help="Show more output.")
+args = parser.parse_args()
 
-def count_jobs(condorPath,seedJobDirectory,seedOutDirectory,seedLogDirectory):
-  failed_count = 0
-  finished_count = 0
-  for seedJob in seedJobDirectory:
-    seed = seedJob.split(".job")[0]
-    seedOut = seed + ".out"
-    seedLog = seed + ".log"
-    if seedLog not in seedLogDirectory:
-      os.system("rm ./condor_log/{}".format(seedJob))
+condor_folder = args.folder
+verbose = args.verbose
+
+def variable_occurence(count_arr,    seed):
+    seed_str = "{:0{}b}".format(seed, len(count_arr))
+    for count, variable in enumerate(seed_str):
+        if variable == "1":
+            count_arr[count] += 1
+    return count_arr
+
+def count_job(condorPath, seedOut, seedLog, seedOutDirectory, failed_count, finished_count):
+    if seedOut in seedOutDirectory:
+        job_success = False
+        for line in open(os.getcwd() + "/" + condor_folder + "/" + seedOut).readlines():
+            if "ROC-integral" in line:
+                job_success = True
+                finished_count += 1
+                return failed_count, finished_count
+        if job_success == False:
+            failed_count += 1
+            if verbose:
+                print("{} failed to compute ROC-integral.".format(seedOut))
+            return failed_count, finished_count
     else:
-      failed_count, finished_count = count_job(condorPath,seedOut,seedLog,seedOutDirectory,failed_count,finished_count)
-  return failed_count, finished_count
-    
-seedDirectory = os.listdir(os.getcwd() + "/condor_log/")
+        condor_done = False
+        for line in open(condorPath + seedLog).readlines():
+            if "005 (" or "009 (" in line: condor_done = True
+            if "006 (" or "000 (" or "001 (" in line: condor_done = False
+        if condor_done == False:
+            return failed_count, finished_count
+        else:
+            failed_count += 1
+            print("{} failed to run.".format(seedLog))
+            return failed_count, finished_count
+
+def count_jobs(condorPath, seedJobDirectory, seedOutDirectory, seedLogDirectory):
+    failed_count = 0
+    finished_count = 0
+    for seedJob in seedJobDirectory:
+        seed = seedJob.split(".job")[0]
+        seedOut = seed + ".out"
+        seedLog = seed + ".log"
+        if seedLog not in seedLogDirectory:
+            os.system("rm ./" + condor_folder + "/{}".format(seedJob))
+        else:
+            failed_count, finished_count = count_job(condorPath, seedOut, seedLog, seedOutDirectory, failed_count, finished_count)
+    return failed_count, finished_count
+        
+seedDirectory = os.listdir(os.getcwd() + "/" + condor_folder + "/")
 seedOutDirectory = [seedStr for seedStr in seedDirectory if ".out" in seedStr]
 seedLogDirectory = [seedStr for seedStr in seedDirectory if ".log" in seedStr]
 seedJobDirectory = [seedStr for seedStr in seedDirectory if ".job" in seedStr]
@@ -56,25 +68,29 @@ numVars = int(seedLogDirectory[0].split("vars_")[0].split("Keras_")[1])
 count_arr = np.zeros(numVars)
 
 total_count = sum(".job" in seedStr for seedStr in seedDirectory)
-failed_count, finished_count = count_jobs(os.getcwd()+"/condor_log/",seedJobDirectory,seedOutDirectory,seedLogDirectory)
+failed_count, finished_count = count_jobs(os.getcwd() + "/" + condor_folder + "/", seedJobDirectory, seedOutDirectory, seedLogDirectory)
+seed_count = 0
 
 for seedName in seedDirectory:
-  if "Subseed" not in seedName and ".job" in seedName:
-    seed = int(seedName.split("_Seed_")[1].split(".job")[0])
-    count_arr = variable_occurence(count_arr,seed)
-    
+    if "Subseed" not in seedName and ".job" in seedName:
+        seed = int(seedName.split("_Seed_")[1].split(".job")[0])
+        count_arr = variable_occurence(count_arr, seed)
+        seed_count += 1
+        
 # display variable frequency 
-print("{:<3} {:<40} {:<6}".format("#","Variable Name","Count"))
+print("{:<3} {:<40} {:<6}".format("#", "Variable Name", "Count"))
 for i in range(numVars):
-  print("{:<3} {:<40} {:<6}".format(str(i)+".",varsList.varList["DNN"][i][0],int(count_arr[i])))
+    print("{:<3} {:<40} {:<6}".format(str(i)+".", varsList.varList["DNN"][i][0], int(count_arr[i])))
 
 # display job status
 print("Finished condor jobs: {} / {}, {:.2f}%".format(
-  finished_count, total_count, 100. * float(finished_count) / float(total_count)
-  )
+    finished_count, total_count, 100. * float(finished_count) / float(total_count)
+    )
 )
 
 print("Failed condor jobs: {} / {}, {:.2f}%".format(
-  failed_count, total_count, 100. * float(failed_count) / float(total_count)
-  )
+    failed_count, total_count, 100. * float(failed_count) / float(total_count)
+    )
 )
+
+print("Submitted {} seeds".format(seed_count))
