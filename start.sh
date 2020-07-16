80502135bdf744204b9c6a0a1c621d815aacaf6c
Adam Furman <furmada@gmail.com>
2020-07-16 15:33:35 -0500

    Changed start.sh to set correct ROOT

--- start.sh
+++ start.sh
@@ -1,6 +1,7 @@
 echo "Setting up TTTT environment"
+export SCRAM_ARCH=slc7_amd64_gcc630
 source /cvmfs/cms.cern.ch/cmsset_default.sh
 cmsenv
-source /cvmfs/sft.cern.ch/lcg/views/LCG_91/x86_64-centos7-gcc62-opt/setup.sh
+source /cvmfs/sft.cern.ch/lcg/app/releases/ROOT/6.16.00/x86_64-centos7-gcc48-opt/bin/thisroot.sh
 echo "Done."
 
