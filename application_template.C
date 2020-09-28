7ec93516351f667d456dd56ba4e5181a51fdf1bb
Daniel Li <45447223+daniel-sunyou-li@users.noreply.github.com>
2020-09-28 11:40:19 -0400

    Add files via upload

---
+++ application_template.C
@@ -0,0 +1,241 @@
+#include <cstdlib>
+#include <vector>
+#include <iostream>
+#include <map>
+#include <string>
+
+#include "TFile.h"
+#include "TTree.h"
+#include "TString.h"
+#include "TSystem.h"
+#include "TROOT.h"
+#include "TStopwatch.h"
+
+#include "TMVA/Tools.h"
+#include "TMVA/Reader.h"
+#include "TMVA/MethodCuts.h"
+
+using namespace TMVA;
+
+void TMVAClassificationApplication( TString myMethodList = "", TString inputFile="", TString outputFile="" ) 
+{   
+   // This loads the library
+   TMVA::Tools::Instance();
+
+   // Default MVA methods to be trained + tested
+   std::map<std::string,int> Use;
+
+   // --- Cut optimisation
+   Use["Cuts"]            = 1;
+   Use["CutsD"]           = 1;
+   Use["CutsPCA"]         = 0;
+   Use["CutsGA"]          = 0;
+   Use["CutsSA"]          = 0;
+   // 
+   // --- 1-dimensional likelihood ("naive Bayes estimator")
+   Use["Likelihood"]      = 1;
+   Use["LikelihoodD"]     = 0; // the "D" extension indicates decorrelated input variables (see option strings)
+   Use["LikelihoodPCA"]   = 1; // the "PCA" extension indicates PCA-transformed input variables (see option strings)
+   Use["LikelihoodKDE"]   = 0;
+   Use["LikelihoodMIX"]   = 0;
+   //
+   // --- Mutidimensional likelihood and Nearest-Neighbour methods
+   Use["PDERS"]           = 1;
+   Use["PDERSD"]          = 0;
+   Use["PDERSPCA"]        = 0;
+   Use["PDEFoam"]         = 1;
+   Use["PDEFoamBoost"]    = 0; // uses generalised MVA method boosting
+   Use["KNN"]             = 1; // k-nearest neighbour method
+   //
+   // --- Linear Discriminant Analysis
+   Use["LD"]              = 1; // Linear Discriminant identical to Fisher
+   Use["Fisher"]          = 0;
+   Use["FisherG"]         = 0;
+   Use["BoostedFisher"]   = 0; // uses generalised MVA method boosting
+   Use["HMatrix"]         = 0;
+   //
+   // --- Function Discriminant analysis
+   Use["FDA_GA"]          = 1; // minimisation of user-defined function using Genetics Algorithm
+   Use["FDA_SA"]          = 0;
+   Use["FDA_MC"]          = 0;
+   Use["FDA_MT"]          = 0;
+   Use["FDA_GAMT"]        = 0;
+   Use["FDA_MCMT"]        = 0;
+   //
+   // --- Neural Networks (all are feed-forward Multilayer Perceptrons)
+   Use["MLP"]             = 1; // Recommended ANN
+   Use["MLPBFGS"]         = 0; // Recommended ANN with optional training method
+   Use["MLPBNN"]          = 0; // Recommended ANN with BFGS training method and bayesian regulator
+   Use["CFMlpANN"]        = 0; // Depreciated ANN from ALEPH
+   Use["TMlpANN"]         = 0; // ROOT's own ANN
+   Use["DNN_CPU"]         = 0; // ROOT's own ANN
+   Use["kDNN"]			  = 0; // Keras DNN
+   //
+   // --- Support Vector Machine 
+   Use["SVM"]             = 1;
+   // 
+   // --- Boosted Decision Trees
+   Use["BDT"]             = 0; // uses Adaptive Boost
+   Use["BDTG"]            = 0; // uses Gradient Boost
+   Use["BDTB"]            = 0; // uses Bagging
+   Use["BDTD"]            = 0; // decorrelation + Adaptive Boost
+   // 
+   // --- Friedman's RuleFit method, ie, an optimised series of cuts ("rules")
+   Use["RuleFit"]         = 1;
+   // ---------------------------------------------------------------
+   Use["Plugin"]          = 0;
+   Use["Category"]        = 0;
+   Use["SVM_Gauss"]       = 0;
+   Use["SVM_Poly"]        = 0;
+   Use["SVM_Lin"]         = 0;
+
+   
+   std::cout << std::endl;
+   std::cout << "Start TMVA Classification Application" << std::endl;
+   
+   // Select methods (don't look at this code - not of interest)
+   if (myMethodList != "") {
+      for (std::map<std::string,int>::iterator it = Use.begin(); it != Use.end(); it++) it->second = 0;
+
+      std::vector<TString> mlist = gTools().SplitString( myMethodList, ',' );
+      for (UInt_t i=0; i<mlist.size(); i++) {
+         std::string regMethod(mlist[i]);
+
+         if (Use.find(regMethod) == Use.end()) {
+            std::cout << "Method \"" << regMethod 
+                      << "\" not known in TMVA under this name. Choose among the following:" << std::endl;
+            for (std::map<std::string,int>::iterator it = Use.begin(); it != Use.end(); it++) {
+               std::cout << it->first << " ";
+            }
+            std::cout << std::endl;
+            return;
+         }
+         Use[regMethod] = 1;
+      }
+   }
+   
+   // --- Create the Reader object
+
+   TMVA::Reader *reader = new TMVA::Reader( "!Color:!Silent" );    
+
+   // Create a set of variables and declare them to the reader
+   // - the variable names MUST corresponds in name and type to those given in the weight file(s) used
+   Float_t var<number>;
+   reader->AddVariable( "<var>", &var<number>);//order needs to match the order in varsList.py
+ 
+   // --- Book the MVA methods
+
+   // Book method(s)
+   reader->BookMVA("kDNN", <weightFilePathMass> );
+   
+   // Prepare input tree (this must be replaced by your data source)
+   // in this example, there is a toy tree with signal and one with background events
+   // we'll later on use only the "signal" events for the test in this example.
+   //   
+   TFile *input(0);
+   
+   if (!gSystem->AccessPathName( inputFile )){ 
+      input = TFile::Open( inputFile ); // check if file in local directory exists 
+      } 
+   if (!input) {
+      std::cout << "ERROR: could not open data file: "<<inputFile << std::endl;
+      exit(1);
+   }
+   std::cout << "--- TMVAClassificationApp    : Using input file: " << input->GetName() << std::endl;
+   
+   // --- Event loop
+
+   // Prepare the event tree
+   // - here the variable names have to correspond to your tree
+   // - you can use the same variables as above which is slightly faster,
+   //   but of course you can use different ones and copy the values inside the event loop
+   //
+   std::cout << "--- Select signal sample" << std::endl;
+   TTree* theTree = (TTree*)input->Get("ljmet");
+   theTree->SetBranchAddress( "<var>", &var<number>);//order needs to match the order in varsList.py
+
+   TFile *target  = new TFile( outputFile,"RECREATE" );
+   target->cd();
+   TTree *newTree = theTree->CloneTree(0);   
+   Float_t kDNN<mass>; TBranch *b_kDNN<mass> = newTree->Branch("kDNN",&kDNN<mass>,"kDNN<mass>/F");
+
+   // Efficiency calculator for cut method
+   Int_t    nSelCutsGA = 0;
+   Double_t effS       = 0.7;
+
+   std::cout << "--- Processing: " << theTree->GetEntries() << " events" << std::endl;
+   TStopwatch sw;
+   sw.Start();
+   for (Long64_t ievt=0; ievt<theTree->GetEntries();ievt++) {
+
+      if (ievt%1000 == 0) std::cout << "--- ... Processing event: " << ievt << std::endl;
+
+      theTree->GetEntry(ievt);
+
+      // --- Return the MVA outputs and fill into histograms
+
+      if (Use["CutsGA"]) {
+         // Cuts is a special case: give the desired signal efficienciy
+         Bool_t passed = reader->EvaluateMVA( "CutsGA method", effS );
+         if (passed) nSelCutsGA++;
+      }
+      
+      DNN<mass> = reader->EvaluateMVA( "kDNN" );
+      
+      newTree->Fill();
+    }
+	
+   // Get elapsed time
+   sw.Stop();
+   std::cout << "--- End of event loop: "; sw.Print();
+
+   // Get efficiency for cuts classifier
+   if (Use["CutsGA"]) std::cout << "--- Efficiency for CutsGA method: " << double(nSelCutsGA)/theTree->GetEntries()
+                                << " (for a required signal efficiency of " << effS << ")" << std::endl;
+
+   if (Use["CutsGA"]) {
+
+      // test: retrieve cuts for particular signal efficiency
+      // CINT ignores dynamic_casts so we have to use a cuts-secific Reader function to acces the pointer  
+      TMVA::MethodCuts* mcuts = reader->FindCutsMVA( "CutsGA method" ) ;
+
+      if (mcuts) {      
+         std::vector<Double_t> cutsMin;
+         std::vector<Double_t> cutsMax;
+         mcuts->GetCuts( 0.7, cutsMin, cutsMax );
+         std::cout << "--- -------------------------------------------------------------" << std::endl;
+         std::cout << "--- Retrieve cut values for signal efficiency of 0.7 from Reader" << std::endl;
+         for (UInt_t ivar=0; ivar<cutsMin.size(); ivar++) {
+            std::cout << "... Cut: " 
+                      << cutsMin[ivar] 
+                      << " < \"" 
+                      << mcuts->GetInputVar(ivar)
+                      << "\" <= " 
+                      << cutsMax[ivar] << std::endl;
+         }
+         std::cout << "--- -------------------------------------------------------------" << std::endl;
+      }
+   }
+
+   // --- Write tree
+   newTree->Write();
+   target->Close();
+
+   std::cout << "--- Created root file: \"TMVApp.root\" containing the MVA output histograms" << std::endl;
+  
+   delete reader;
+    
+   std::cout << "==> TMVAClassificationApplication is done!" << std::endl << std::endl;
+} 
+	
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
