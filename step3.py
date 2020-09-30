ea8201b36b42fd169ccbd6322d4a2df2cd6cc2a3
Daniel Li <45447223+daniel-sunyou-li@users.noreply.github.com>
2020-09-30 16:41:46 -0400

    Update step3.py

--- step3.py
+++ step3.py
@@ -3,61 +3,66 @@
 import os, sys, glob
 from argparse import ArgumentParser
 import numpy as np
-from ROOT import TMVA, TFile
+from ROOT import TFile, TTree
 from array import array
 from json import loads as load_json
-import varsList
+import os, sys
 
-TMVA.Tools.Instance()
-TMVA.PyMethodBase.PyInitialize()
+os.environ["KERAS_BACKEND"] = "tensorflow"
+
+import keras
+from keras.models import Sequential
+from keras.layers.core import Dense, Dropout
+from keras.layers import BatchNormalization
+from keras.optimizers = import Adam
+from sklearn.metrics import roc_auc_score, roc_curve, auc
+
+import varsList
 
 # read in arguments
 parser = ArgumentParser()
 parser.add_argument("-i","--inputDir",required=True)
 parser.add_argument("-f","--fileName",required=True)
 parser.add_argument("-o","--outName",required=True)
-
 args = parser.parse_args()
 
-# load json parameters file
+# load the .json parameters
 jsonFile = None
 jsonCheck = glob.glob("parameters*.json")
 if len(jsonCheck) > 0:
     jsonFile = open(jsonCheck[0])
     jsonFile = load_json(jsonFile.read())
+# load in the model
+model = None
+modelCheck = glob.glob("*.h5")
+if len(modelCheck) > 0:
+    model = keras.models.load_model(modelCheck[0])
     
 variables = jsonFile[list(jsonFile.keys())[0]]["parameters"]["variables"]
+
+print("Using {} variables".format(len(variables)))
+for i, variable in enumerate(variables): print(" {:<4} {}".format(i+".",variable))
 
 inputDir = args.inputDir
 fileName = args.fileName
 outName  = args.outName
 
-reader = TMVA.Reader("Color:!Silent")
+# load in the sample
 
-file = TFile.Open(inputDir + "/" + fileName)
-tree = file.Get("ljmet")
+rootFile = TFile.Open(inputDir + "/" + fileName)
+rootTree = rootFile.Get("ljmet")
+events = np.asarray( rootTree.AsMatrix( variables ) )
 
-branches = {}
-
-for var in varsList.varList["DNN"]:
-    if var[0] in variables:
-        branches[var[0]] = array( "f", [-999] )
-        reader.AddVariable( var[0], branches[var[0]] )
-        tree.SetBranchAddress( var[0], branches[var[0]] )
+discriminators = model.predict(events)
 
 out = TFile( outName, "RECREATE" );
 out.cd()
-new_tree = tree.CloneTree(0);
+new_tree = rootTree.CloneTree(0);
 
-DNN_disc = array("f", [0])
-new_tree.Branch( "DNN_disc", DNN_disc, "DNN_disc/F" );
+new_tree.Branch( "DNN_disc", discriminators, "DNN_disc/F" );
 
-reader.BookMVA( "PyKeras", TString("weights.xml") )
-
-for i in range(tree.GetEntries()):
-    tree.GetEntry(i)
-    DNN_disc[0] = reader.EvaluateMVA( "PyKeras" )
+for i in range(rootTree.GetEntries()):
     new_tree.Fill()
     
-new_tree.Write()
+out.Write()
 out.Close()
