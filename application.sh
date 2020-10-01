1f35cebbc97698d525bea0d6379458e9f4ed7206
Daniel Li <45447223+daniel-sunyou-li@users.noreply.github.com>
2020-10-01 15:26:26 -0400

    Update application.sh

--- application.sh
+++ application.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
-inputDir=${1}
+condorDir=${1}
 fileName=${2}
 outputDir=${3}
 
 source /cvmfs/cms.cern.ch/cmsset_default.sh
 source /cvmfs/sft.cern.ch/lcg/contrib/gcc/7.3.0/x86_64-centos7-gcc7-opt/setup.sh
 
-python step3.py -i $inputDir -f $fileName -o $outputDir
+python step3.py -i $condorDir -f $fileName -o $outputDir
