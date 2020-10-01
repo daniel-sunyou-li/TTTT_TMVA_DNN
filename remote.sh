bebfdfe731a4d648188875e32529b610882fca12
Daniel Li <45447223+daniel-sunyou-li@users.noreply.github.com>
2020-10-01 16:48:52 -0400

    Update remote.sh

--- remote.sh
+++ remote.sh
@@ -17,30 +17,6 @@ rm CMSSW946.tgz
 
 cd ./CMSSW_9_4_6_patch1/src/TTTT_TMVA_DNN
 
-# Identify and Copy Signal and Background Samples
-
-signal=""
-bkgrnd=""
-if [ $year = "2017" ]; then
-	signal=`python -c 'import sys; sys.path.insert(0, "../TTTT_TMVA_DNN"); import varsList; print(" ".join(varsList.sig2017_0));'`
-	bkgrnd=`python -c 'import sys; sys.path.insert(0, "../TTTT_TMVA_DNN"); import varsList; print(" ".join(varsList.bkg2017_0));'`
-elif [ $year = "2018" ]; then
-	signal=`python -c 'import sys; sys.path.insert(0, "../TTTT_TMVA_DNN"); import varsList; print(" ".join(varsList.sig2018_0));'`
-	bkgrnd=`python -c 'import sys; sys.path.insert(0, "../TTTT_TMVA_DNN"); import varsList; print(" ".join(varsList.bkg2018_0));'`
-fi
-
-for sig in $signal:
-do
-	xrdcp -s root://cmseos.fnal.gov//store/user/$eos_username/$eos_folder/${sig//:} .
-done
-for bkg in $bkgrnd:
-do
-	xrdcp -s root://cmseos.fnal.gov//store/user/$eos_username/$eos_folder/${bkg//:} .
-done
-
-# Change to TTTT folder
-cd ..
-
 export SCRAM_ARCH=slc7_amd64_gcc630
 eval `scramv1 runtime -sh`
 source /cvmfs/sft.cern.ch/lcg/app/releases/ROOT/6.16.00/x86_64-centos7-gcc48-opt/bin/thisroot.sh
