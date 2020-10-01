51c7a6255bdf4826473b1590d85a6774ab308c69
Daniel Li <45447223+daniel-sunyou-li@users.noreply.github.com>
2020-10-01 17:18:52 -0400

    Create application.sh

---
+++ application.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+condorDir=${1}
+fileName=${2}
+outputDir=${3}
+eosUserName=${4}
+
+xrdcp -s root://cmseos.fnal.gov//store/user/$eosUserName/CMSSW946.tgz .
+tar -xf CMSSW946.tgz
+rm CMSSW946.tgz
+
+mv *.h5 CMSSW_9_4_6_patch1/src/TTTT_TMVA_DNN/
+mv *.json CMSSW_9_4_6_patch1/src/TTTT_TMVA_DNN/
+
+cd CMSSW_9_4_6_patch1/src/TTTT_TMVA_DNN
+
+source /cvmfs/cms.cern.ch/cmsset_default.sh
+source /cvmfs/cms.cern.ch/lcg/views/LCG_94/x86_64-centos7-gcc8-opt/setup.sh
+
+python step3.py -i $condorDir -f $fileName -o $outputDir
