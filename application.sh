a56d8e67158967eb17b5a6cdb0d81a13d3591c8e
Daniel Li <45447223+daniel-sunyou-li@users.noreply.github.com>
2020-10-01 15:04:06 -0400

    Update application.sh

--- application.sh
+++ application.sh
@@ -1,19 +1,10 @@
 #!/bin/sh
 
 inputDir=${1}
-resultDir=${2}
-fileName=${3}
-condorDir=${4}
-outName=$fileName\_step3.root
+fileName=${2}
+outputDir=${3}
 
 source /cvmfs/cms.cern.ch/cmsset_default.sh
-export SCRAM_ARCH=slc7_amd64_gcc630
-eval `scramv1 runtime -sh`
-
 source /cvmfs/sft.cern.ch/lcg/contrib/gcc/7.3.0/x86_64-centos7-gcc7-opt/setup.sh
-source /cvmfs/sft.cern.ch/lcg/app/releases/ROOT/6.16.00/x86_64-centos7-gcc48-opt/bin/thisroot.sh
 
-python step3.py -i $inputDir -f $fileName -o $outName
-
-echo Transferring $outName to $inputDir/step3/ ...
-cp $outName $inputDir/step3/
+python step3.py -i $inputDir -f $fileName -o $outputDir
