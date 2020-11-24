9ce2e015fed81e98e64e99c881b479568a038d80
dsunyou <daniel_li2@brown.edu>
2020-11-24 00:07:12 -0600

    update correlation script

--- correlation.py
+++ correlation.py
@@ -9,154 +9,147 @@ import varsList
 TMVA.Tools.Instance()
 TMVA.PyMethodBase.PyInitialize()
 
-# Load parameters
-weight_string = varsList.weightStr
-cut_string = TCut(varsList.cutStr)
+def get_correlation_matrix(year, variables, njets, nbjets):
+  # Returns the correlation matrix of the given variables
+  # Get signal and background paths
+  signal_path = os.path.join(os.getcwd(),
+                             varsList.step2Sample[ str(year) ] + "/nominal/",
+                             varsList.sig_training[ str(year) ][0] )
+  bkgrnd_path = os.path.join(os.getcwd(),
+                             varsList.step2Sample[ str(year) ] + "/nominal/",
+                             varsList.bkg_training[ str(year) ][0] )
 
-def get_correlation_matrix(year, variables):
-    # Returns the correlation matrix of the given variables
-    # Get signal and background paths
-    signal_path = os.path.join(os.getcwd(),
-                               varsList.step2Sample2017 if year == 2017 else varsList.step2Sample2018,
-                               varsList.sig2017_0[0] if year == 2017 else varsList.sig2018_0[0])
-    bkgrnd_path = os.path.join(os.getcwd(),
-                               varsList.step2Sample2017 if year == 2017 else varsList.step2Sample2018,
-                               varsList.bkg2017_0[0] if year == 2017 else varsList.bkg2018_0[0])
+  # Create TMVA object
+  loader = TMVA.DataLoader("tmva_data")
 
-    # Create TMVA object
-    loader = TMVA.DataLoader("tmva_data")
+  # Load used variables
+  for var in variables:
+    try:
+      var_data = varsList.varList["DNN"][ [ v[0] for v in varsList.varList["DNN"] ].index(var) ]
+      loader.AddVariable( var_data[0], var_data[1], "", "F" )
+    except ValueError:
+      print( "[WARN] The variable {} was not found. Omitting.".format(var) )
 
-    # Load used variables
-    for var in variables:
-        try:
-            var_data = varsList.varList["DNN"][[v[0] for v in varsList.varList["DNN"]].index(var)]
-            loader.AddVariable(var_data[0], var_data[1], var_data[2], "F")
-        except ValueError:
-            print("[WARN] The variable {} was not found. Omitting.".format(var))
+  # Open ROOT files
+  signal_f = TFile.Open( signal_path )
+  signal = signal_f.Get( "ljmet" )
+  bkgrnd_f = TFile.Open( bkgrnd_path )
+  bkgrnd = bkgrnd_f.Get( "ljmet" )
 
-    # Open ROOT files
-    signal_f = TFile.Open(signal_path)
-    signal = signal_f.Get("ljmet")
-    bkgrnd_f = TFile.Open(bkgrnd_path)
-    bkgrnd = bkgrnd_f.Get("ljmet")
+  # Load signal and background
+  loader.AddSignalTree(signal)
+  loader.fTreeS = signal
+  loader.AddBackgroundTree(bkgrnd)
+  loader.fTreeB = bkgrnd
 
-    # Load signal and background
-    loader.AddSignalTree(signal)
-    loader.fTreeS = signal
-    loader.AddBackgroundTree(bkgrnd)
-    loader.fTreeB = bkgrnd
+  # Set weights
+  weight_string = varsList.weightStr
+  loader.SetSignalWeightExpression( weight_string )
+  loader.SetBackgroundWeightExpression( weight_string )
 
-    # Set weights
-    loader.SetSignalWeightExpression(weight_string)
-    loader.SetBackgroundWeightExpression(weight_string)
-
-    # Set cuts
-    loader.PrepareTrainingAndTestTree(
-        cut_string, cut_string,
-        "nTrain_Signal=0:nTrain_Background=0:SplitMode=Random:NormMode=NumEvents:!V:VerboseLevel=Info"
-    )
+  # Set cuts
+  cutStr = varsList.cutStr
+  cutStr += " && ( NJetsCSVwithSF_MultiLepCalc >= {} )".format( nbjets ) 
+  cutStr += " && ( NJets_JetSubCalc >= {} )".format( njets )
+  cutStr += " && ( ( isTraining == 1 ) || ( isTraining == 2 ) )"
+  cut_string = TCut( cutStr )
+  loader.PrepareTrainingAndTestTree(
+    cut_string, cut_string,
+    "nTrain_Signal=0:nTrain_Background=0:SplitMode=Random:NormMode=NumEvents:!V:VerboseLevel=Info"
+  )
     
-    # Set the pointer to the right histogram
-    loader.GetDefaultDataSetInfo().GetDataSet().GetEventCollection()
+  # Set the pointer to the right histogram
+  loader.GetDefaultDataSetInfo().GetDataSet().GetEventCollection()
     
-    # Retrieve the signal correlation matrix
-    sig_th2 = loader.GetCorrelationMatrix("Signal")
+  # Retrieve the signal correlation matrix
+  sig_th2 = loader.GetCorrelationMatrix( "Signal" )
 
-    n_bins = sig_th2.GetNbinsX()
-    sig_corr = np.zeros((n_bins, n_bins))
+  n_bins = sig_th2.GetNbinsX()
+  sig_corr = np.zeros( ( n_bins, n_bins ) )
     
-    for x in range(n_bins):
-        for y in range(n_bins):
-            sig_corr[x, y] = sig_th2.GetBinContent(x + 1, y + 1)
+  for x in range(n_bins):
+    for y in range(n_bins):
+      sig_corr[x, y] = sig_th2.GetBinContent(x + 1, y + 1)
     
-    return sig_corr
+  return sig_corr
 
-def reweight_importances(year, variables, importances):
-    # Re-weight the variable importances
-    corr_mat = abs(get_correlation_matrix(int(year), variables)) / 100
-    row_sum_inv_sq = (corr_mat.sum(axis=1))**(-2)
-    weight_mat = np.array([
-        np.multiply(row_sum_inv_sq[i], corr_mat[i]) for i in range(len(corr_mat))
-    ])
-    weighted_sig = np.dot(weight_mat, importances)
-    return weighted_sig
-
-def reweight_importances(year, variables, importances):
-    # Re-weight the variable importances
-    corr_mat = abs(get_correlation_matrix(int(year), variables) / 100.0)
-    mod_corr_mat = np.zeros((len(corr_mat), len(corr_mat)))
-    for i in range(len(corr_mat)):
-        for j in range(len(corr_mat)):
-            if i == j:
-	        	mod_corr_mat[i,j] = corr_mat[i,j]
-            elif j > i:
-                mod_corr_mat[i,j] = corr_mat[i,j]
-                for k in range(j):
-                    if k > i:
-                        mod_corr_mat[i,j] *= 1.0 - corr_mat[k,j]
+def reweight_importances( year, variables, importances, njets, nbjets ):
+  # Re-weight the variable importances
+  corr_mat = abs( get_correlation_matrix( int(year), variables, njets, nbjets ) / 100.0 )
+  mod_corr_mat = np.zeros( ( len( corr_mat ), len( corr_mat ) ) )
+  for i in range(len(corr_mat)):
+    for j in range(len(corr_mat)):
+      if i == j:
+     	mod_corr_mat[i,j] = corr_mat[i,j]
+      elif j > i:
+        mod_corr_mat[i,j] = corr_mat[i,j]
+        for k in range(j):
+          if k > i:
+            mod_corr_mat[i,j] *= 1.0 - corr_mat[k,j]
     
-    wgt_sig_mat = np.array([ np.multiply(importances,mod_corr_mat[i]) for i in range(len(corr_mat)) ])
-    weightLSig = np.zeros(len(corr_mat))
-    weightQSig = np.zeros(len(corr_mat))
-    for i in range(len(corr_mat)):
-        for j in range(len(corr_mat)):
-            if i == j:
-                weightLSig[i] = wgt_sig_mat[i,j]
-                weightQSig[i] = wgt_sig_mat[i,j]**2
-            if j > i:
-                weightLSig[i] -= wgt_sig_mat[i,j]
-                weightQSig[i] -= wgt_sig_mat[i,j]**2
+  wgt_sig_mat = np.array([ np.multiply(importances,mod_corr_mat[i]) for i in range(len(corr_mat)) ])
+  weightLSig = np.zeros(len(corr_mat))
+  weightQSig = np.zeros(len(corr_mat))
+  for i in range(len(corr_mat)):
+    for j in range(len(corr_mat)):
+      if i == j:
+        weightLSig[i] = wgt_sig_mat[i,j]
+        weightQSig[i] = wgt_sig_mat[i,j]**2
+      if j > i:
+        weightLSig[i] -= wgt_sig_mat[i,j]
+        weightQSig[i] -= wgt_sig_mat[i,j]**2
     
-    print("Weighted Quadratic Sum: {}".format(np.sum(np.sqrt(weightQSig))))
-    print("Weighted Linear Sum: {}".format(np.sum(weightLSig)))
-    return weightLSig, np.sqrt(weightQSig)
+  print("Weighted Quadratic Sum: {}".format(np.sum(np.sqrt(weightQSig))))
+  print("Weighted Linear Sum: {}".format(np.sum(weightLSig)))
+  return weightLSig, np.sqrt(weightQSig)
 
 def get_correlated_groups(corr_mat, variables, cutoff):
-    # Returns the groups of variables which are <cutoff> or more correlated
-    shape = np.shape(corr_mat)
+  # Returns the groups of variables which are <cutoff> or more correlated
+  shape = np.shape(corr_mat)
 
-    # Find pairs of correlated variables
-    pairs = []
-    for i in range(shape[0]):
-        for j in range(i+1, shape[1]):
-			if abs(corr_mat[i,j]) >= cutoff:
-				pairs.append([variables[i],variables[j]])
+  # Find pairs of correlated variables
+  pairs = []
+  for i in range(shape[0]):
+    for j in range(i+1, shape[1]):
+      if abs(corr_mat[i,j]) >= cutoff:
+        pairs.append([variables[i],variables[j]])
 
-    # Find correlated groups
-    groups = pairs
-	for pair in pairs:
-		for i, group in enumerate(groups):
-			if len( set(pair) & set(group) ) > 0:
-				groups[i] = sorted(list(set(group+pair)))
+  # Find correlated groups
+  groups = pairs
+  for pair in pairs:
+    for i, group in enumerate(groups):
+      if len( set(pair) & set(group) ) > 0:
+        groups[i] = sorted(list(set(group+pair)))
 				
-	correlated_groups = list(set(tuple(group) for group in groups))
-	return correlated_groups, pairs
+  correlated_groups = list(set(tuple(group) for group in groups))
+  return correlated_groups, pairs
                     
-def generate_uncorrelated_seeds(count, variables, cutoff, year):
-    # Generates <count> uncorrelated Seed objects using the specified variables
-    # Get correlated pairs of variables
-    groups, _ = get_correlated_groups(
-        get_correlation_matrix(year, variables),
-        variables,
-        cutoff)
+def generate_uncorrelated_seeds(count, variables, cutoff, year, njets, nbjets):
+  # Generates <count> uncorrelated Seed objects using the specified variables
+  # Get correlated pairs of variables
+  groups, _ = get_correlated_groups(
+    get_correlation_matrix(year, variables, njets, nbjets),
+    variables,
+    cutoff
+  )
 
-    # Generate some random seeds
-    seeds = [Seed.random(variables) for _ in range(count)]
+  # Generate some random seeds
+  seeds = [Seed.random(variables) for _ in range( int(count) ) ]
 
-    # Find seeds that violate correlated pairs and modify them
+  # Find seeds that violate correlated pairs and modify them
 
-    for seed in seeds:
-        for group in groups:
-            # group = {X, Y, Z} are highly correlated
-            group_vars = [v for v in group if seed.includes(v)]
-            # group_vars = {X, Z} -> variables from <group> which are ON in seed (seed.includes(Y) == False)
-            if len(group_vars) > 1:
-                # This seed needs to be modified.
-                keep = randint(0, len(group_vars))
-                # Pick a random variable from {X, Z} to keep: for example Z (1).
-                for i, gv in enumerate(group_vars):
-                    if i != keep:
-                        # for i == 0 (X) -> exclude X from seed
-                        seed.exclude(gv)
+  for seed in seeds:
+    for group in groups:
+      # group = {X, Y, Z} are highly correlated
+      group_vars = [v for v in group if seed.includes(v)]
+      # group_vars = {X, Z} -> variables from <group> which are ON in seed (seed.includes(Y) == False)
+      if len(group_vars) > 1:
+        # This seed needs to be modified.
+        keep = randint(0, len(group_vars))
+        # Pick a random variable from {X, Z} to keep: for example Z (1).
+        for i, gv in enumerate(group_vars):
+          if i != keep:
+            # for i == 0 (X) -> exclude X from seed
+            seed.exclude(gv)
 
-    return seeds
+  return seeds
