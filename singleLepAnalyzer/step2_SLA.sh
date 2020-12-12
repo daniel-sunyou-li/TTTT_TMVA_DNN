092853c2a8d6d8fe9cc6eee9be07130291c2f500
Daniel Li <45447223+daniel-sunyou-li@users.noreply.github.com>
2020-12-11 22:50:14 -0500

    Create step2_SLA.sh

---
+++ singleLepAnalyzer/step2_SLA.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+YEAR=${1}
+EOSDIR=${2}
+EOS_USERNAME=${3}
+
+source /cvmfs/cms.cern.ch/cmsset_default.sh
+
+xrdcp -s root://cmseos.fnal.gov//store/user/$EOS_USERNAME/CMSSW946.tgz
+tar -xf CMSSW946.tgz
+rm CMSSW946.tgz
+
+cd ./CMSSW_9_4_6_patch1/src/TTTT_TMVA_DNN
+
+eval `scramv1 runtime -sh`
+
+python templates.py -y $YEAR
