d2a512b1a0356df87ac6063378eef4ef6045e045
Daniel Li <45447223+daniel-sunyou-li@users.noreply.github.com>
2021-01-16 12:44:36 -0500

    Create setupBRUX.py

---
+++ setup/setupBRUX.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+import os, sys, getpass, pexpect, argparse
+sys.path.insert(0, "../TTTT_TMVA_DNN")
+import varsList
+
+parser = ArgumentParser()
+parser.add_argument( "-y", "--year", required = True )
+parser.add_argument( "-t", "--tag", default = "" )
+args = parser.parse_args()
+
+samples = varsList.all_samples[ args.year ]
+
+for sample in samples:
+  os.system( "mkdir ./nominal/")
+  os.system(  "xrdcp  root://cmseos.fnal.gov///store/user/{}/{}nominal/{} ./nominal/".format( varsList.eosUserName, varsList.step3Sample[ args.year ] + args.tag, sample )
+  for syst in [ "JEC", "JER" ]:
+    for dir in [ "up", "down" ]:
+      os.system( "mkdir ./{}".format( syst + dir ) )
+      if "up" in sample.lower() or "down" in sample.lower(): continue
+      if "muon" in sample.lower() or "electron" in sample.lower() or "egamma" in sample.lower() or "jetht" in sample.lower(): continue
+      os.system( xrdcp root://cmseos.fnal.gov///store/user/{}/{}{}/{} .{}/".format( varsList.eosUserName, varsList.step3Sample[ args.year } + args.tag, syst + dir, sample, syst + dir )
+  
