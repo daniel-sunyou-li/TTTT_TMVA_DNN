370a66d8aa0575d4bc6fae2be835e34b952aff2d
Daniel Li <45447223+daniel-sunyou-li@users.noreply.github.com>
2021-01-16 18:14:51 -0500

    Update setupBRUX.py

--- setup/setupBRUX.py
+++ setup/setupBRUX.py
@@ -13,6 +13,14 @@ all_samples = varsList.all_samples[ args.year ]
 samples = [ all_samples[ sample_key ][0] for sample_key in all_samples.key() ]
 
 out_folder = varsList.step3Sample[ args.year ] + args.tag 
+done_samples = {
+  "nominal": os.listdir( out_folder + "/nominal/" ),
+}
+for syst in [ "JEC", "JER" ]:
+  for dir in [ "up", "down" ]:
+    done_samples[ syst + dir ] = os.listdir( out_folder + "/{}".format( syst + dir ) )
+  
+
 
 if not os.path.exists( out_folder ):
   os.system( "mkdir ./{}".format( out_folder ) )
@@ -23,10 +31,13 @@ if not os.path.exists( out_folder ):
             
             
 for sample in samples:
-  os.system(  "xrdcp  root://cmseos.fnal.gov///store/user/{}/{}nominal/{} ./nominal/".format( varsList.eosUserName, out_folder, sample )
+  if sample not in done_samples[ "nominal" ]: 
+    print( "Adding {}".format( sample ) )
+    os.system(  "xrdcp  root://cmseos.fnal.gov///store/user/{}/{}nominal/{} ./nominal/".format( varsList.eosUserName, out_folder, sample )
   for syst in [ "JEC", "JER" ]:
     for dir in [ "up", "down" ]:
       if "up" in sample.lower() or "down" in sample.lower(): continue
       if "muon" in sample.lower() or "electron" in sample.lower() or "egamma" in sample.lower() or "jetht" in sample.lower(): continue
-      os.system( xrdcp root://cmseos.fnal.gov///store/user/{}/{}{}/{} .{}/".format( varsList.eosUserName, out_folder, syst + dir, sample, syst + dir )
+      if sample not in done_samples[ syst + dir ]:
+        os.system( xrdcp root://cmseos.fnal.gov///store/user/{}/{}{}/{} .{}/".format( varsList.eosUserName, out_folder, syst + dir, sample, syst + dir )
   
