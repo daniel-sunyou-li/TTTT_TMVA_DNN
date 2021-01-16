096b1a8650512149835c26a7c06676b0d78491b2
Daniel Li <45447223+daniel-sunyou-li@users.noreply.github.com>
2021-01-16 12:56:45 -0500

    Update setupBRUX.py

--- setup/setupBRUX.py
+++ setup/setupBRUX.py
@@ -11,12 +11,18 @@ args = parser.parse_args()
 
 samples = varsList.all_samples[ args.year ]
 
+if not os.path.exists( varsList.step3Sample[ args.year ] + args.tag ):
+  os.system( "mkdir ./{}".format( varsList.step3Sample[ args.year ] + args.tag ) )
+  os.system( "mkdir ./{}/nominal/".format( varsList.step3Sample[ args.year ] + args.tag ) )
+  for syst in [ "JEC", "JER" ]:
+    for dir in [ "up", "down" ]:
+      os.system( "mkdir ./{}/{}".format( varsList.step3Sample[ args.year ] + args.tag, syst + dir ) )
+            
+            
 for sample in samples:
-  os.system( "mkdir ./nominal/")
   os.system(  "xrdcp  root://cmseos.fnal.gov///store/user/{}/{}nominal/{} ./nominal/".format( varsList.eosUserName, varsList.step3Sample[ args.year ] + args.tag, sample )
   for syst in [ "JEC", "JER" ]:
     for dir in [ "up", "down" ]:
-      os.system( "mkdir ./{}".format( syst + dir ) )
       if "up" in sample.lower() or "down" in sample.lower(): continue
       if "muon" in sample.lower() or "electron" in sample.lower() or "egamma" in sample.lower() or "jetht" in sample.lower(): continue
       os.system( xrdcp root://cmseos.fnal.gov///store/user/{}/{}{}/{} .{}/".format( varsList.eosUserName, varsList.step3Sample[ args.year } + args.tag, syst + dir, sample, syst + dir )
