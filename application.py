0ee46775c76950a5331a7bd6bcc381eea4c655ef
Daniel Li <45447223+daniel-sunyou-li@users.noreply.github.com>
2021-01-22 22:13:34 -0500

    Update application.py

--- application.py
+++ application.py
@@ -55,14 +55,14 @@ if args.resubmit != None:
   # check for failed jobs based on .out file -- ran, but failed during filling tree in step3.py
   for out_file in out_files:
     lines = open( os.path.join( args.resubmit, out_file ) ).readlines()
-      if "[OK ]" not in lines[-1]:
-        sample_name = out_file.split( "hadd" )[0] + "hadd.root" 
-        sample_tag = out_file.split( "hadd_" )[1].split( "." )[0] 
-        if sample_tag not in submit_files.keys():
-          submit_files[ sample_tag ] = [ sample_name ]
-        else: submit_files[ sample_tag ].append( sample_name )
-          resubmit_count += 1
-        if args.verbose: print( ">> Resubmitting failed job: {}".format( sample_name ) )
+    if "[OK ]" not in lines[-1]:
+      sample_name = out_file.split( "hadd" )[0] + "hadd.root" 
+      sample_tag = out_file.split( "hadd_" )[1].split( "." )[0] 
+      if sample_tag not in submit_files.keys():
+        submit_files[ sample_tag ] = [ sample_name ]
+      else: submit_files[ sample_tag ].append( sample_name )
+        resubmit_count += 1
+      if args.verbose: print( ">> Resubmitting failed job: {}".format( sample_name ) )
   # check for failed jobs based on .log and .out file -- job was held due to insufficient memory requested, resubmit with more memory
   for log_file in log_files:
     if log_file.split( "." )[0] + ".out" not in out_files:
